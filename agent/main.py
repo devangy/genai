e4a4dd144b0dacb435bb219e859a1d4701ed094d
devangnpm <d3vang2002@gmail.com>
2026-05-11 20:41:41 +0530

    tools

--- agent/main.py
+++ agent/main.py
@@ -3,19 +3,51 @@ import os
 from google.genai import types
 from dotenv import load_dotenv
 from google.genai.types import Part
-from rich import print_json
+from rich import console, print_json
+from rich.markdown import Markdown
+from rich.console import Console
 
-class Agent:
+
+
+class CalculatorTool():
+    """A tool for performing mathematical calculations"""
+
+    def get_schema(self):
+        return {
+            "name": "calculator",
+            "description" : "performs basic mathematical calculations. use for a simple addition multiplication etc",
+            "parameters" : {
+                "type" : "object",
+                "properties" : {
+                    "expression" : {
+                        "type" : "STRING",
+                        "description" : "The mathematical expression to evaluate eg ('2+2', '10+5', '5*22')"
+                    }
+                },
+                "required" :     ["expression"],
+            }
+        }
+
+    
+
+class Agent: 
     """A simple AI agent that can answer questions in a multi-turn conversation"""
 
     def __init__(self):
         self.client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
-        self.model =  "gemini-2.5-flash"
-        self.system_message = "You are a helpful assistant that breaks down problems into steps and solves them systematically"
+        self.model =  "gemini-3-flash-preview"
+        self.system_message = "You are a helpful expert assistant that breaks down problems into steps and solves them systematically."
         # short term memory
         self.messages = []
+        self.console = Console()
+        self.tools = []
 
     
+    def _get_tool_schemas():
+
+
+
+
     def chat(self, message):
         
         """Process a user message and return a response"""
@@ -23,27 +55,36 @@ class Agent:
         # inject user message
         self.messages.append({
             "role" : "user",
-            "content" : message
+            "parts" : [{"text": message}] 
         })
 
+        
         response = self.client.models.generate_content(
             model=self.model,
-            contents=types.Part.from_text(text=message),
+            contents=self.messages,
             config=types.GenerateContentConfig(
-                temperature = 0,
-                top_p = 0.95,
-                top_k = 20,
+                system_instruction= self.system_message,
+                temperature = 0.8,
+                tools=self.tools,
+               # top_p = 0.95,
+                #top_k = 20,
             )
         )
+        print(dir(response))
+
 
 
         # inject llm response
         self.messages.append({
-            "role" : "assistant",
-            "content" : response.text
+            "role" : "model",
+            "parts" : [{"text": message}]
         })
 
         return response
+
+    def render(self, text):
+        md = Markdown(text)
+        self.console.print(md)
 
     def list_available_models(self):
         print("Available Models:")  
@@ -54,23 +95,29 @@ class Agent:
     
 
 
+
 def main():
 
     load_dotenv()
+    
 
     print("Agent starting!")
     
     agent = Agent()
 
-    
-
     #print(agent.list_available_models())
 
-    response1 =  agent.chat("I have 5 apples. How many do you have?")
-    print_json(data=response1.text)
+    res1 =  agent.chat("I have 5 apples. How many do you have?")
+    agent.render(res1.text)
 
-    response2 = agent.chat("i ate 1 apple. how many are left now?")
-    print_json(data=response2.text)
+
+
+    res2 = agent.chat("i ate 1 apple. how many are left now?")
+    agent.render(res2.text)
+
+    res3 = agent.chat("what is 157.09 * 493.89")
+    agent.render(res3.text)
+
 
 
 
